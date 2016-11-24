d7bcb216b9626662f020ed22823ad6fb7cca3056
Szczyp <qb@szczyp.com>
2016-11-24 15:42:00 +0100

    Set line buffering on stdin

--- launcher/Main.hs
+++ launcher/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Protolude
+import Protolude hiding (drop)
 
 import Data.String (String)
 import System.Process (callProcess)
@@ -10,19 +10,22 @@ import Data.Text (unpack)
 import qualified System.IO.Streams as Streams
 import Control.Monad.Trans.Maybe
 import Control.Error.Util
+import Data.ByteString (drop)
+import System.IO
 
 player :: String
 player = "mpv"
 
-options :: [String]
-options = ["--no-terminal"
-          ,"--volume=70"
-          ]
+defaultOptions :: [String]
+defaultOptions = ["--no-terminal"
+                 ,"--volume=70"
+                 ]
 
 main :: IO ()
-main = run $
-  read >> read
-  >>= play . (: options) . unpack . view (key "link" . _String)
-  where read = hoistMaybe =<< (liftIO $ Streams.read Streams.stdin)
+main = run $ play . options =<< read
+  where read = hoistMaybe =<< liftIO (Streams.read Streams.stdin)
         play = liftIO . callProcess player
-        run a = runMaybeT a >> return ()
+        options = (: defaultOptions) . unpack . view (key "link" . _String) . drop 4
+        run a = do
+          hSetBuffering stdin LineBuffering
+          void $ runMaybeT a
