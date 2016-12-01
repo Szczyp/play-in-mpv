b4ae7a9ff2ee29b4f0fdd81250065842a2bd3f57
Szczyp <qb@szczyp.com>
2016-12-01 05:19:00 +0100

    Fix length decoding

--- launcher/src/Main.hs
+++ launcher/src/Main.hs
@@ -2,43 +2,43 @@
 
 module Main where
 
-import Protolude hiding (StateT, for, fromStrict, stdin, stdout)
+import Protolude hiding (StateT, fromStrict, for, stdin)
 
-import Control.Error.Util
-import Control.Lens
-import Control.Monad.Trans.Maybe
-import Data.Aeson.Lens
-import Data.Binary
-import Data.ByteString           (pack)
+import Control.Error.Util (hoistMaybe)
+import Control.Lens (view)
+import Control.Monad.Trans.Maybe (MaybeT, runMaybeT)
+import Data.Aeson.Lens (key, _String)
+import Data.Binary.Get (runGet, getInt32host)
+import Data.ByteString           (ByteString, pack)
 import Data.ByteString.Lazy      (fromStrict)
-import Data.Text                 hiding (pack)
-import Pipes
-import Pipes.ByteString          hiding (pack, unpack, unwords)
-import Pipes.Parse
-import System.Process            (callCommand)
+import Data.Text                 (Text, unpack, unwords)
+import Pipes (Producer, runEffect, for)
+import Pipes.ByteString          (drawByte, stdin)
+import Pipes.Parse (StateT, parsed_)
+import System.Process            (spawnCommand)
 
 player :: Text
 player = "mpv"
 
 defaultOptions :: [Text]
 defaultOptions = ["--no-terminal"
-                 ,"--volume 80"
-                 ,"--ytdl-format 'bestvideo[height<=?1080]+bestaudio'"
-                 ,"--ytdl-raw-options mark-watched=,cookie=~/Downloads/cookies"
+                 ,"--volume=80"
+                 ,"--ytdl-format=\"bestvideo[height<=?1080]+bestaudio\""
+                 ,"--ytdl-raw-options=mark-watched=,cookie=~/Downloads/cookies"
                  ]
 
 drawBytes :: Monad m => Int -> MaybeT (StateT (Producer ByteString m x) m) ByteString
 drawBytes n = do
   draws <- lift $ replicateM n drawByte
-  pack <$> (hoistMaybe $ sequence draws)
+  pack <$> hoistMaybe (sequence draws)
 
 readUrl :: Monad m => MaybeT (StateT (Producer ByteString m x) m) Text
 readUrl = do
-  n <- decode @Int32 . fromStrict <$> drawBytes 4
+  n <- runGet getInt32host . fromStrict <$> drawBytes 4
   view (key "link" . _String) <$> drawBytes (fromIntegral n)
 
 main :: IO ()
-main = void $ runEffect $ for urls $ play . options
+main = void $ runEffect $ for urls $ void . play . options
   where urls = parsed_ (runMaybeT readUrl) stdin
-        play = lift . callCommand . unpack . unwords . (player :)
-        options url = defaultOptions <> ["'" <> url <> "'"]
+        play = lift . spawnCommand . unpack . unwords . (player :)
+        options url = defaultOptions <> ["\"" <> url <> "\""]
