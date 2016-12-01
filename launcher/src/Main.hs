f9e529dc67c735084ed5e84a7b26962d6fd67d1a
Szczyp <qb@szczyp.com>
2016-12-01 14:14:47 +0100

    Support youtube playlists

--- launcher/src/Main.hs
+++ launcher/src/Main.hs
@@ -1,21 +1,23 @@
-{-# LANGUAGE TypeApplications #-}
-
 module Main where
 
-import Protolude hiding (StateT, for, fromStrict, stdin)
+import Protolude hiding (StateT, for, hush, list, stdin, to, (&))
 
-import Control.Error.Util        (hoistMaybe)
-import Control.Lens              (view)
-import Control.Monad.Trans.Maybe (MaybeT, runMaybeT)
-import Data.Aeson.Lens           (key, _String)
-import Data.Binary.Get           (getInt32host, runGet)
-import Data.ByteString           (ByteString, pack)
-import Data.ByteString.Lazy      (fromStrict)
-import Data.Text                 (Text, unpack, unwords)
-import Pipes                     (Producer, for, runEffect)
-import Pipes.ByteString          (drawByte, stdin)
-import Pipes.Parse               (StateT, parsed_)
-import System.Process            (callCommand)
+import Control.Error.Util
+import Control.Lens                     hiding (index)
+import Control.Monad.Trans.Maybe        (MaybeT, runMaybeT)
+import Data.Aeson.Lens
+import Data.Attoparsec.ByteString       (parseOnly)
+import Data.Attoparsec.ByteString.Char8 (decimal)
+import Data.Binary.Get                  (getInt32host, runGet)
+import Data.ByteString                  (pack)
+import Data.List                        (lookup)
+import Data.Map.Strict                  (fromList)
+import Data.Text                        (unwords)
+import Pipes                            (Producer, for, runEffect)
+import Pipes.ByteString                 (drawByte, stdin)
+import Pipes.Parse                      (StateT, parsed_)
+import System.Process                   (callCommand)
+import URI.ByteString
 
 
 player :: Text
@@ -30,16 +32,36 @@ defaultOptions = ["--no-terminal"
 
 readUrl :: Monad m => MaybeT (StateT (Producer ByteString m x) m) Text
 readUrl = do
-  n <- runGet getInt32host . fromStrict <$> drawBytes 4
+  n <- runGet getInt32host . toS <$> drawBytes 4
   view (key "link" . _String) <$> drawBytes (fromIntegral n)
-  where drawBytes n = do
-          draws <- lift $ replicateM n drawByte
-          pack <$> hoistMaybe (sequence draws)
+  where drawBytes n = replicateM n (lift drawByte)
+                      >>= hoistMaybe . sequence
+                      <&> pack
+
+optionHandlers :: Map ByteString (URI -> Maybe [Text])
+optionHandlers = fromList [("www.youtube.com", youtubeOptions)]
+
+youtubeOptions :: URI -> Maybe [Text]
+youtubeOptions uri = do
+  let query = uri ^. queryL . queryPairsL
+  list <- query & lookup "list"
+  index <- lookup "index" query >>= hush . parseOnly decimal :: Maybe Int
+  let url = uri
+            & queryL . queryPairsL .~ [("list", list)]
+            & pathL .~ "/playlist"
+            & serializeURIRef'
+  return $ defaultOptions |> "--playlist-start=" <> show (index - 1) |> toS url
+
+specialOptions :: Text -> Maybe [Text]
+specialOptions url = do
+  uri <- hush $ parseURI strictURIParserOptions (toS url)
+  host <- uri ^? authorityL . _Just . authorityHostL . hostBSL
+  optionHandlers ^. at host . _Just . to ($ uri)
 
 main :: IO ()
 main = void $ runEffect $ for urls $ play . options
   where urls = parsed_ (runMaybeT readUrl) stdin
-        play = lift . spawn . unpack . unwords . (player :)
-        options url = defaultOptions <> ["\"" <> url <> "\""]
+        play = lift . spawn . toS . unwords . cons player
+        options url = specialOptions url ?: defaultOptions |> url
         spawn = void . forkIO . callCommand
 
