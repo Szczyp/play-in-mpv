18fd445bae98b6a84afdf294470a9c106b2c93b3
Szczyp <qb@szczyp.com>
2016-12-01 14:17:41 +0100

    Remove nodejs launcher

--- native/install.bat
+++
@@ -1,2 +0,0 @@
-REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.szczyp.play_in_mpv" /ve /t REG_SZ /d "%~dp0com.szczyp.play_in_mpv.json" /f
-REG ADD "HKCU\Software\Mozilla\NativeMessagingHosts\com.szczyp.play_in_mpv" /ve /t REG_SZ /d "%~dp0com.szczyp.play_in_mpv.json" /f
--- native/uninstall.bat
+++
@@ -1,2 +0,0 @@
-REG DELETE "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.szczyp.play_in_mpv" /f
-REG DELETE "HKCU\Software\Mozilla\NativeMessagingHosts\com.szczyp.play_in_mpv" /f
